98e554fceadd4cabbea98e7141c175c418ac4f19
Lauren Richert <lsrichert@gmail.com>
2018-05-24 15:35:59 -0500

    added exercises

---
+++ main.js
@@ -0,0 +1,8 @@
+let cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 0
+
+for (let y = 0; y < cookies.length; y++) {
+    const currentCookie = cookies[y]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
+
