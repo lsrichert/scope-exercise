2381449c4961b3ca5a203fabb48643883171bec5
Lauren Richert <lsrichert@gmail.com>
2018-05-29 09:02:43 -0500

    added simon says

--- main.js
+++ main.js
@@ -49,3 +49,23 @@ ModSquad.members.forEach(member => {
 })
 
 console.log(HTMLRepresentation)
+
+
+
+// this is simon says exercise from Scope
+
+const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+for (let k = 0; k < locations.length; k++) {
+    let currentLocation = locations[k]
+    let invalidLocation = false;
+
+    if (currentLocation[0] > 2) {
+         invalidLocation = true
+    }
+
+    if (invalidLocation) {
+        console.log("This location is invalid")
+    }
+    console.log(`There were ${k} locations displayed`)
+}
