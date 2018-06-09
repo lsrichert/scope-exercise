5a8c262cc611acfa0fab24d92cb590d227bbd641
Lauren Richert <lsrichert@gmail.com>
2018-06-09 16:43:23 -0500

    added Lambda Llama from Fri Fundamentals

--- main.js
+++ main.js
@@ -69,3 +69,21 @@ for (let k = 0; k < locations.length; k++) {
     }
     console.log(`There were ${k} locations displayed`)
 }
+
+// this is Lambda Llama
+
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+  
+    const namer = function () {
+      const suffix = " the Llama"
+      const name = possibleNames[randomizer]
+      return name + suffix;
+    }
+    let newName = namer();
+    return newName;
+  }
+  
+  nameMaker = llamaNamer();
+  console.log(nameMaker)
